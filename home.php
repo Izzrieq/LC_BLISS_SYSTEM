cce27e7c2ab3873317aa38b34ffcb73f7a5bae06
Izzrieq <izzrieqilhan@gmail.com>
2023-08-07 09:51:30 +0800

    home.php

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+ </head>
+ <body>
+    <header>
+        <div class="left-nav">
+            <img src="assets/LC_COMPANY LOGO_MARCH 2023-01.png" alt="logo">
+        </div>
+        <div class="right-nav">
+            <h1>bliss customer service e-log</b></h1>
+        </div>
+    </header>
+        <div class="homebodycard">
+            <div class="card-box">
+
+            </div>
+            <div class="card-box">
+                
+            </div>
+            <div class="card-box">
+                
+            </div>
+        </div>
+
+    <footer>
+        <center>
+          <p>© 2023 Little Caliph International</p>
+        </center>
+    </footer>
+ </body>
+ </html>
--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
  </head>
  <body>
     <header>
@@ -30,11 +32,13 @@
      	<label>User Password</label> <br>
      	<input type="password" name="password" ><br>
             <br>
-     	<button type="submit">Login</button>
+         <button type="submit" class="btn btn-primary">Login</button>
      </form>
      </center>
     <footer>
-        <p>copyright</p>
+        <center>
+          <p>© 2023 Little Caliph International</p>
+        </center>
     </footer>
  </body>
  </html>
