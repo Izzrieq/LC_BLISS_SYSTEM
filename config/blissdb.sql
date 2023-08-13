f249700c2f21ae0a8c6ca83d747880c903b6948f
Nasy08 <[anassuhaimi08@gmail.com]>
2023-08-13 11:22:33 +0800

    new database

--- config/blissdb.sql
+++ config/blissdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2023 at 08:41 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Aug 13, 2023 at 05:20 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,7 +33,7 @@ CREATE TABLE `best` (
   `contact` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `alamat` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -53,7 +53,7 @@ CREATE TABLE `complaintbliss` (
   `lcowner` varchar(25) DEFAULT NULL,
   `ownernohp` varchar(15) NOT NULL,
   `status` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `complaintbliss`
@@ -64,6 +64,36 @@ INSERT INTO `complaintbliss` (`id`, `date`, `cname`, `cnohp`, `category`, `type`
 (2, '2023-08-09 09:03:16', 'anas', '012-234-6789', 'complaint', 'management', '            ccxAXaxaxxa', 'kelantan', 'coqan', '012-333-4444', NULL),
 (3, '2023-08-09 09:06:44', 'testingg', '011-222-3333', 'complaint', 'sales', '            sgsfsf', 'johor', 'testing123', '012-345-6789', NULL),
 (4, '2023-08-09 09:26:47', 'Anas', '019-470-0983', 'suggestion', 'sales', 'kkjkhjk', 'LC SHAH ALAM', 'IZZ', '011-222-3020', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datalc`
+--
+
+CREATE TABLE `datalc` (
+  `id` int(3) NOT NULL,
+  `stateid` varchar(10) NOT NULL,
+  `bizstype` varchar(2) NOT NULL,
+  `lcid` varchar(255) NOT NULL,
+  `operatorname` varchar(255) NOT NULL,
+  `ownername` varchar(255) NOT NULL,
+  `eduemail` varchar(255) NOT NULL,
+  `kindernohp` varchar(14) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `datalc`
+--
+
+INSERT INTO `datalc` (`id`, `stateid`, `bizstype`, `lcid`, `operatorname`, `ownername`, `eduemail`, `kindernohp`) VALUES
+(1, 'SEL', 'B', 'LC SEL Jalan Pualam', 'Sabariah Faridah Binti Jamaluddin', 'Sabariah Faridah Binti Jamaluddin', 'k.seksyen7@littlecaliphs.edu.my', '0133625593'),
+(2, 'SEL', 'L', 'LC SEL Taman TTDI Jaya', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'ttdi jaya@littlecaliphs.edu.my', '0133625593'),
+(3, 'SEL', 'L', 'LC SEL Jalan Platinum', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'jlnplatinum@littlecaliphs.edu.my', '019-9471707'),
+(4, 'SEL', 'L', 'LC SEL Setia Alam', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'setiaalam@littlecaliphs.edu.my', '0123852696'),
+(5, 'SEL', 'L', 'LC SEL Bandar Seri Putra', 'Waffa Binti A. Rahim', 'Waffa Binti A. Rahim', 'bdrseriputra@littlecaliphs.edu.my', '0192072057'),
+(6, 'MLK', 'L', 'LC MLK Sungai Udang', 'Siti Affiza Binti Mohammad', 'Siti Affiza Binti Mohammad', 'sgudang@littlecaliphs.edu.my', '0196008564'),
+(7, 'SEL', 'L', 'LC SEL Bandar Puteri, Klang', 'Tadika Prestij Khalifa Sdn Bhd', 'Norizan Binti Abdul Rasid', 'bdrputeri.klang01@littlecaliphs.edu.my', '0123432760');
 
 -- --------------------------------------------------------
 
@@ -95,17 +125,17 @@ CREATE TABLE `lcdetails` (
   `type` varchar(255) NOT NULL,
   `operatornohp` varchar(255) NOT NULL,
   `operatoraddress` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `lcdetails`
 --
 
 INSERT INTO `lcdetails` (`id`, `stateid`, `bizstype`, `lcid`, `operatorname`, `ownername`, `status`, `yearsigned`, `datesigned`, `dateoperated`, `tlcppackage`, `annuallicense`, `eduemail`, `kindername`, `kindernohp`, `noblock`, `street`, `postcode`, `city`, `state`, `type`, `operatornohp`, `operatoraddress`) VALUES
-(1, 'SEL', 'B', 'LC SEL Jalan Pualam', 'Sabariah Faridah Binti Jamaluddin', 'Sabariah Faridah Binti Jamaluddin', 'Operate', '2005', '0000-00-00', '0000-00-00', ' -   ', ' -   ', 'k.seksyen7@littlecaliphs.edu.my', 'Tadika Rama Rama Bistari', '013-362 5593', 'No 2, Jalan Pualam 2 7/32B, Seksyen 7, 40000 Shah Alam, Selangor', '', 0, '', '', 'Extension D', '013-2896500', 'No. 4, Bukit Raja Business Park, Jalan Keluli 7/109, 40000 Shah Alam, Selangor'),
-(2, 'SEL', 'L', 'LC SEL Taman TTDI Jaya', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'Operate', '2009', '0000-00-00', '0000-00-00', '', ' 10 ', 'ttdijaya@littlecaliphs.edu.my', 'Tadika Khalifah Cilik Mayang Sari', '012-2198234', 'No.1, Jalan Esei 1 U2/41A, Taman TTDI Jaya, 40150 Shah Alam, Selangor', '', 0, '', '', 'Extension D', '012-3852696', 'No. 9, Jalan Adang U8/17, Bukit Jelutong, 40150 Shah Alam, Selangor'),
-(3, 'SEL', 'L', 'LC SEL Jalan Platinum', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'Operate', '2009', '0000-00-00', '0000-00-00', '', ' 10 ', 'jlnplatinum@littlecaliphs.edu.my', 'Tadika Anak Bestari Platinum', '019-9471707', 'No. 28, Jalan Platinum 7/43D, Seksyen 7, 40000 Shah Alam, Selangor', '', 0, '', '', 'Extension D', '012-3852696', 'No. 9, Jalan Adang U8/17, Bukit Jelutong, 40150 Shah Alam, Selangor'),
-(4, 'SEL', 'L', 'LC SEL Setia Alam', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'Operate', '2009', '0000-00-00', '0000-00-00', '', ' 10 ', 'setiaalam@littlecaliphs.edu.my', 'Tadika Khalifah Impian', '012-3852696', 'No. 1, Jalan Setia Impian U13/6A, Seksyen U13, 40170 Shah Alam, Selangor', '', 0, '', '', 'Extension D', '012-3852696', 'No. 9, Jalan Adang U8/17, Bukit Jelutong, 40150 Shah Alam, Selangor');
+(1, 'SEL', 'B', 'LC SEL Jalan Pualam', 'Sabariah Faridah Binti Jamaluddin', 'Sabariah Faridah Binti Jamaluddin', 'Operate', 2005, '0000-00-00', '0000-00-00', ' -   ', ' -   ', 'k.seksyen7@littlecaliphs.edu.my', 'Tadika Rama Rama Bistari', '013-362 5593', 'No 2, Jalan Pualam 2 7/32B, Seksyen 7, 40000 Shah Alam, Selangor', '', 0, '', '', 'Extension D', '013-2896500', 'No. 4, Bukit Raja Business Park, Jalan Keluli 7/109, 40000 Shah Alam, Selangor'),
+(2, 'SEL', 'L', 'LC SEL Taman TTDI Jaya', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'Operate', 2009, '0000-00-00', '0000-00-00', '', ' 10 ', 'ttdijaya@littlecaliphs.edu.my', 'Tadika Khalifah Cilik Mayang Sari', '012-2198234', 'No.1, Jalan Esei 1 U2/41A, Taman TTDI Jaya, 40150 Shah Alam, Selangor', '', 0, '', '', 'Extension D', '012-3852696', 'No. 9, Jalan Adang U8/17, Bukit Jelutong, 40150 Shah Alam, Selangor'),
+(3, 'SEL', 'L', 'LC SEL Jalan Platinum', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'Operate', 2009, '0000-00-00', '0000-00-00', '', ' 10 ', 'jlnplatinum@littlecaliphs.edu.my', 'Tadika Anak Bestari Platinum', '019-9471707', 'No. 28, Jalan Platinum 7/43D, Seksyen 7, 40000 Shah Alam, Selangor', '', 0, '', '', 'Extension D', '012-3852696', 'No. 9, Jalan Adang U8/17, Bukit Jelutong, 40150 Shah Alam, Selangor'),
+(4, 'SEL', 'L', 'LC SEL Setia Alam', 'T. Jamaliah Binti Tengku Ismail', 'T. Jamaliah Binti Tengku Ismail', 'Operate', 2009, '0000-00-00', '0000-00-00', '', ' 10 ', 'setiaalam@littlecaliphs.edu.my', 'Tadika Khalifah Impian', '012-3852696', 'No. 1, Jalan Setia Impian U13/6A, Seksyen U13, 40170 Shah Alam, Selangor', '', 0, '', '', 'Extension D', '012-3852696', 'No. 9, Jalan Adang U8/17, Bukit Jelutong, 40150 Shah Alam, Selangor');
 
 -- --------------------------------------------------------
 
@@ -121,7 +151,7 @@ CREATE TABLE `operator` (
   `email` varchar(255) NOT NULL,
   `detailcomplain` longtext NOT NULL,
   `datetime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -133,7 +163,7 @@ CREATE TABLE `user` (
   `id` int(3) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
@@ -158,6 +188,13 @@ ALTER TABLE `best`
 -- Indexes for table `complaintbliss`
 --
 ALTER TABLE `complaintbliss`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `lcid` (`lcid`);
+
+--
+-- Indexes for table `datalc`
+--
+ALTER TABLE `datalc`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `lcid` (`lcid`);
 
